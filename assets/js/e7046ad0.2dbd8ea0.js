d35825e3b94a3cc34cf23f8a8f2a4de0b2c2c17b
Camusat Antoine <antoine.camusat@insa-rouen.fr>
2022-10-06 23:10:48 -0400

    website

---
+++ assets/js/e7046ad0.2dbd8ea0.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[7699],{536:function(e){e.exports=JSON.parse('{"permalink":"/lib2ddoc/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
